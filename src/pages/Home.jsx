bbc35303a86272059bcd582552e39cd00c710aca
vandanapumaggi <vandanapumaggi@gmail.com>
2026-09-08 20:00:09 +0530

    fixed home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
+import { Link } from "react-router-dom";
+import heroImg from "../assets/hero.png"; // if error, change to ../assets/images/hero.png
 import "./Home.css";
-function Home() {
+
+const Home = () => {
     return (
-        <div className="home-simple">
-            <h1>Welcome to Movie Explorer</h1>
+        <div className="home-bg" style={{ backgroundImage: `url(${heroImg})` }}>
+            <div className="home-overlay">
+
+                {/* Navbar centered */}
+                <nav className="navbar">
+                    <Link to="/">Home</Link>
+                    <span>|</span>
+                    <Link to="/movies">Movies</Link>
+                </nav>
+
+                {/* Center content */}
+                <div className="center-content">
+                    <h1>Welcome to Movie Explorer</h1>
+                    <p>Explore thousands of movies</p>
+                    <Link to="/movies" className="explore-btn">Explore Movies</Link>
+                </div>
+
+            </div>
         </div>
     );
-}
+};
+
 export default Home;
